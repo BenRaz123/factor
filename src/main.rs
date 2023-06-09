6edd14c81220cdbc958cc8cb2f4d952681f85a52
BenRaz123 <ben.raz2008@gmail.com>
2023-06-09 09:01:58 -0400

    Organized Code Better
    Changed the clap builder to a clap derive and refactored an if
    statement`
    
    Signed-off-by: BenRaz123 <ben.raz2008@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,33 @@
-use clap::*;
+use clap::{Parser, ArgAction::SetTrue};
+
+#[derive(Parser, Debug)]
+#[command(name="factor", author="Ben Raz <ben.raz2008@gmail.com>")]
+struct Args {
+    #[arg(help="An unsigned integer")]
+    number: u64,
+    #[arg(short, long)]
+    #[arg(help="toggles human-readable output", action=SetTrue)]
+    fancy: Option<bool>
+}
 
 fn main() {
-    let arguments = Command::new("factor")
-        .author("Ben Raz <ben.raz2008@gmail.com>")
-        .arg(
-            Arg::new("number")
-                .required(true)
-                .help("An integer")
-                .value_parser(value_parser!(u64)),
-        )
-        .arg(
-            Arg::new("fancy")
-                .long("fancy")
-                .short('f')
-                .required(false)
-                .help("toggles whether you want human-readable output.")
-                .action(ArgAction::SetTrue),
-        )
-        .get_matches();
 
-    let user_number: u64 = *arguments
-        .get_one("number")
-        .expect("failed to retrieve number!");
+    let args = Args::parse();
 
-    let fancy: bool = *arguments
-        .get_one("fancy")
-        .expect("could not get argument fancy");
+    let user_number: u64 = args.number;
+
+    let fancy: bool = match args.fancy {
+        Some(value) => value,
+        None => false
+    };
 
     let mut list_of_used_multiples: Vec<u64> = vec![];
 
     for number in 1..user_number {
-        if !(user_number % number == 0)
-            || (list_of_used_multiples.contains(&(user_number / number)))
+        let is_divisible_by_current_number: bool = user_number % number == 0;
+        let already_divided = list_of_used_multiples.contains(&(user_number / number));
+
+        if !is_divisible_by_current_number || already_divided
         {
             continue;
         }
